3012c6e315413503b940a5f88a9bee51ec834ba0
Lester <lstr@LSTR.local>
2013-11-07 11:07:16 -0500

    Delete Oldest Files

--- ventaPollos/autoCarga.php
+++
@@ -1,8 +0,0 @@
-<?php
-date_default_timezone_set("America/Lima");
-function __autoload($className)
-{
-	require("clases/$className.php");
-}
-require("libs/FirePHPCore/FirePHP.class.php");
-?>
--- ventaPollos/ofuscador.php
+++
@@ -1,6 +0,0 @@
-<?php
-require("../recursos/jsmin-1.1.1.php");
-
-echo JSMIN::minify(file_get_contents('funcionesJS.js'))
-
-?>
